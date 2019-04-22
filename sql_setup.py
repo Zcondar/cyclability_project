c4aecfb5229c376b5042c7fb2ad84128144fed81
Unknown <wndtanaka@gmail.com>
2019-04-22 19:51:46 +1000

    sql_setup.py completed It create a table and insert contents from given csv files.

--- sql_setup.py
+++ sql_setup.py
@@ -1,6 +1,5 @@
 import psycopg2
 import csv
-import pprint
 
 def pgconnect():
     try:
@@ -34,47 +33,6 @@ def pgexec(conn, sqlcmd, args, msg, silent=False):
                     print(e)
     return retval
 
-def clean_empty_string(data):
-    for row in data:
-        for key, value in row.items():
-            if value == "":
-                row[key] = None
-
-business_stats_data = list(csv.DictReader(open('BusinessStats.csv')))
-print(business_stats_data[-5])
-
-clean_empty_string(business_stats_data)
-
-print(business_stats_data[-5])
-
-
-# 1st: login to database
-conn = pgconnect()
-
-# if you want to reset the table
-pgexec(conn, "DROP TABLE IF EXISTS BusinessStats", None, "Reset Table BusinessStats")
-
-# 2nd: ensure that the schema is in place
-
-business_stats_schema = """CREATE TABLE IF NOT EXISTS BusinessStats (
-                            area_id INT NOT NULL PRIMARY KEY,
-                            num_businesses INT DEFAULT 0,
-                            retail_trade INT DEFAULT 0,
-                            accommodation_and_food_services INT DEFAULT 0,
-                            health_care_and_social_assistance INT DEFAULT 0,
-                            education_and_training INT DEFAULT 0,
-                            arts_and_recreation_services INT DEFAULT 0
-                   )"""
-pgexec(conn, business_stats_schema, None, "Create Table BusinessStats")
-
-# 3nd: load data
-# IMPORTANT: make sure the header line of CSV is without spaces!
-
-insert_stmt = """INSERT INTO BusinessStats(area_id,num_businesses,retail_trade,accommodation_and_food_services,health_care_and_social_assistance,education_and_training,arts_and_recreation_services) VALUES (%(area_id)s, %(num_businesses)s, %(retail_trade)s, %(accommodation_and_food_services)s,%(health_care_and_social_assistance)s, %(education_and_training)s, %(arts_and_recreation_services)s)"""
-
-for row in business_stats_data:
-    pgexec(conn, insert_stmt, row, "row inserted")
-
 
 def pgquery(conn, sqlcmd, args, silent=False):
     """ utility function to execute some SQL query statement
@@ -100,8 +58,145 @@ def pgquery(conn, sqlcmd, args, silent=False):
     return retval
 
 
-# check content of Organisations table
-query_stmt = "SELECT * FROM BusinessStats"
-print(query_stmt)
-pgquery(conn, query_stmt, None)
-conn.close()
+def clean_empty_string(data):
+    for row in data:
+        for key, value in row.items():
+            if value == "":
+                row[key] = None
+
+
+def create_table(file, queries):
+    data = list(csv.DictReader(open(file + '.csv')))
+    clean_empty_string(data)
+    # to reset table
+    pgexec(conn, "DROP TABLE IF EXISTS " + file, None, "Reset Table " + file)
+    # create table using schema
+    pgexec(conn, queries[0], None, "Create Table " + file)
+    # insert values to table
+    for row in data:
+        pgexec(conn, queries[1], row, "row inserted")
+
+
+if __name__ == "__main__":
+    # login to database
+    conn = pgconnect()
+
+    # =====Queries for creating and inserting table=====
+    business_stats_schema = """CREATE TABLE IF NOT EXISTS BusinessStats (
+                                        area_id INT NOT NULL PRIMARY KEY,
+                                        num_businesses INT,
+                                        retail_trade INT,
+                                        accommodation_and_food_services INT,
+                                        health_care_and_social_assistance INT,
+                                        education_and_training INT,
+                                        arts_and_recreation_services INT
+                               )"""
+    business_stats_insert_stmt = """INSERT INTO BusinessStats(
+                                            area_id,
+                                            num_businesses,
+                                            retail_trade,
+                                            accommodation_and_food_services,
+                                            health_care_and_social_assistance,
+                                            education_and_training,
+                                            arts_and_recreation_services)
+                                            VALUES (
+                                            %(area_id)s,
+                                            %(num_businesses)s,
+                                            %(retail_trade)s,
+                                            %(accommodation_and_food_services)s,
+                                            %(health_care_and_social_assistance)s,
+                                            %(education_and_training)s,
+                                            %(arts_and_recreation_services)s)"""
+
+    bike_pods_schema = """CREATE TABLE IF NOT EXISTS BikeSharingPods(
+                            station_id INT NOT NULL PRIMARY KEY,
+                            name VARCHAR(70),
+                            num_bikes INT,
+                            num_scooters INT,
+                            latitude FLOAT,
+                            longitude FLOAT,
+                            description VARCHAR(500)
+                        )"""
+    bike_pods_insert_stmt = """INSERT INTO BikeSharingPods(
+                                station_id,
+                                name,
+                                num_bikes,
+                                num_scooters,
+                                latitude,
+                                longitude,
+                                description)
+                                VALUES (
+                                %(station_id)s,
+                                %(name)s,
+                                %(num_bikes)s,
+                                %(num_scooters)s,
+                                %(latitude)s,
+                                %(longitude)s,
+                                %(description)s)"""
+
+    census_stats_schema = """CREATE TABLE IF NOT EXISTS CensusStats(
+                                area_id INT NOT NULL PRIMARY KEY,
+                                median_annual_household_income INT,
+                                avg_monthly_rent INT
+                            )"""
+    census_stats_insert_stmt = """INSERT INTO CensusStats(
+                                    area_id,
+                                    median_annual_household_income,
+                                    avg_monthly_rent)
+                                    VALUES (
+                                    %(area_id)s,
+                                    %(median_annual_household_income)s,
+                                    %(avg_monthly_rent)s)"""
+
+    neighbourhoods_schema = """CREATE TABLE IF NOT EXISTS Neighbourhoods(
+                                area_id INT NOT NULL PRIMARY KEY,
+                                area_name VARCHAR(70),
+                                land_area FLOAT,
+                                population INT,
+                                number_of_dwellings INT,
+                                number_of_businesses INT
+                            )"""
+    neighbourhoods_insert_stmt = """INSERT INTO Neighbourhoods(
+                                    area_id,
+                                    area_name,
+                                    land_area,
+                                    population,
+                                    number_of_dwellings,
+                                    number_of_businesses)
+                                    VALUES (
+                                    %(area_id)s,
+                                    %(area_name)s,
+                                    %(land_area)s,
+                                    %(population)s,
+                                    %(number_of_dwellings)s,
+                                    %(number_of_businesses)s)"""
+
+    statistical_areas_schema = """CREATE TABLE IF NOT EXISTS StatisticalAreas(
+                                    area_id INT NOT NULL PRIMARY KEY,
+                                    area_name VARCHAR(70),
+                                    parent_area_id INT
+                                )"""
+    statistical_areas_insert_stmt = """INSERT INTO StatisticalAreas(
+                                        area_id,
+                                        area_name,
+                                        parent_area_id)
+                                        VALUES (
+                                        %(area_id)s,
+                                        %(area_name)s,
+                                        %(parent_area_id)s)"""
+    # =====QUERIES END=====
+
+    # Queries stored in key value pair, with file name as key, and queries stored as lists
+    queries = {
+        'BusinessStats': [business_stats_schema, business_stats_insert_stmt],
+        'BikeSharingPods': [bike_pods_schema, bike_pods_insert_stmt],
+        'CensusStats': [census_stats_schema, census_stats_insert_stmt],
+        'Neighbourhoods': [neighbourhoods_schema, neighbourhoods_insert_stmt],
+        'StatisticalAreas': [statistical_areas_schema, statistical_areas_insert_stmt]
+    }
+
+    # Loop through each file and queries to create a table
+    for k, v in queries.items():
+        create_table(k, v)
+    
+    conn.close()
